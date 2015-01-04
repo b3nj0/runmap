052b5fe862073fca29c98f15581e4372ef8176e2
Benjamin Jenks <jenksb@gmail.com>
2015-01-04 18:52:59 +0000

    core.clj: extract list of lat lngs from a fit file and calculate the max and min latlngs

--- src/runmap/core.clj
+++ src/runmap/core.clj
@@ -10,26 +10,45 @@
   (proxy [com.garmin.fit.RecordMesgListener] []
     (onMesg [m] (apply fn m args))))
 
-(defn collect [msgs]
+(defn collect [msgs f]
+  "add messages transformed by f to msgs"
   (fn [m]
-    (swap! msgs conj m)))
+    (swap! msgs conj (f m))))
 
 (defn record-collector [msgs]
-  (record-mesg-listener (collect msgs)))
+  (record-mesg-listener (collect msgs identity)))
 
-(defn parse [file listeners]
+(defn parse [file lnrs]
+  "parse file using listeners lnrs"
   (let [dec (com.garmin.fit.Decode.)
         brd (com.garmin.fit.MesgBroadcaster. dec)
         fin (jio/input-stream file)]
-    (doall (map #(.addListener brd %) listeners))
+    (doall (map #(.addListener brd %) lnrs))
     (.run brd fin)))
 
 (defn record-mesgs [file]
+  "extract RecordMesgs from file"
   (let [msgs (atom [])]
-    (parse file (collect msgs))
+    (parse file [(record-collector msgs)])
     @msgs))
 
+(defn latlngs [recs]
+  "extract lat and lngs from seq of RecordMesgs"
+  (map #(assoc {} :lat (.getPositionLat %1) :lng (.getPositionLong %1)) recs))
+
+(defn bounds [lls]
+  "bounding box for lat lngs"
+  (let [lats (remove nil? (map :lat lls))
+        lngs (remove nil? (map :lng lls))
+        latmin (apply min lats)
+        latmax (apply max lats)
+        lngmin (apply min lngs)
+        lngmax (apply max lngs)]
+    {:latmin latmin :latmax latmax :lngmin lngmin :lngmax lngmax}))
+
 (defn -main [& args]
-  (let [dir "resources"
-        files (fit-files dir)]
-    (println files)))
+  (let [dir "resources/fit-files"
+        files (take 2 (fit-files dir))
+        recs (mapcat record-mesgs files)
+        lls (latlngs recs)]
+    (count lls)))
