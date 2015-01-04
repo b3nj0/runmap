95060b6c69d461a883cc4f0628408640fc186bca
Benjamin Jenks <jenksb@gmail.com>
2015-01-04 22:59:29 +0000

    core.clj: prototype run mapping

--- src/runmap/core.clj
+++ src/runmap/core.clj
@@ -34,7 +34,10 @@
 
 (defn latlngs [recs]
   "extract lat and lngs from seq of RecordMesgs"
-  (map #(assoc {} :lat (.getPositionLat %1) :lng (.getPositionLong %1)) recs))
+  (->> recs
+       (map #(assoc {} :lat (.getPositionLat %1) :lng (.getPositionLong %1)))
+       (filter (comp not nil? :lat))
+       (filter (comp not nil? :lng))))
 
 (defn bounds [lls]
   "bounding box for lat lngs"
@@ -44,11 +47,39 @@
         latmax (apply max lats)
         lngmin (apply min lngs)
         lngmax (apply max lngs)]
-    {:latmin latmin :latmax latmax :lngmin lngmin :lngmax lngmax}))
+    {:min {:lat latmin :lng lngmin} :max {:lat latmax :lng lngmax}}))
+
+(defn scale [val domain range]
+  (let [[dommin dommax] domain
+        dom (- dommax dommin)
+        [rngmin rngmax] range
+        rng (double (- rngmax rngmin))]
+    (+ (* (- val dommin) (/ rng dom)) rngmin)))
+
+(defn scale-latlngs [lls x y]
+  (let [bnds (bounds lls)
+        latdom [(get-in bnds [:min :lat]) (get-in bnds [:max :lat])]
+        lngdom [(get-in bnds [:min :lng]) (get-in bnds [:max :lng])]
+        latrng [0 x]
+        lngrng [0 y]]
+    (map #(vector (scale (:lat %1) latdom latrng) (scale (:lng %1) lngdom lngrng)) lls)))
+
+(defn runmap [files dim]
+  (let [recs (mapcat record-mesgs files)
+        lls (latlngs recs)
+        lls (scale-latlngs lls (:x dim) (:y dim))]
+    lls))
+
+(defn runmap->bitmap [rm dim]
+  (let [img (java.awt.image.BufferedImage. (:x dim) (:y dim) java.awt.image.BufferedImage/TYPE_BYTE_GRAY)
+        g (.createGraphics img)]
+    (doall (map (fn [[x y]] (.drawRect g x y 1 1)) rm))
+    img))
 
 (defn -main [& args]
   (let [dir "resources/fit-files"
-        files (take 2 (fit-files dir))
-        recs (mapcat record-mesgs files)
-        lls (latlngs recs)]
-    (count lls)))
+        files (fit-files dir)
+        dim {:x 2048 :y 2048}
+        rm (runmap files dim)
+        bmp (runmap->bitmap rm dim)]
+    (javax.imageio.ImageIO/write bmp "png" (file "runmap.png"))))
